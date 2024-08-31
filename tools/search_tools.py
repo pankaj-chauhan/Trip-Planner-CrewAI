9dd21fd5d826855a1e79142cb563b8ba880908e5
Pankaj Chauhan <pankajchauhan.687@gmail.com>
2024-08-31 13:29:40 +0530

    updated search.py

--- tools/search_tools.py
+++ tools/search_tools.py
@@ -18,4 +18,20 @@ class SearchTools():
             'X-API-KEY': os.environ['SERPER_API_KEY'],
             'content-type': 'application/json'
         }
-        
+        response = requests.request("POST", url, headers=headers, data=payload)
+        # check if there is an organic key
+        if 'organic' not in response.json():
+            return "Sorry, I couldn't find anything about that, there could be an error with you serper api key."
+        else:
+            results = response.json()['organic']
+            string = []
+            for result in results[:top_result_to_return]:
+                try:
+                    string.append('\n'.join([
+                        f"Title: {result['title']}", f"Link: {result['link']}",
+                        f"Snippet: {result['snippet']}", "\n-----------------"
+                    ]))
+                except KeyError:
+                    next
+
+            return '\n'.join(string)
