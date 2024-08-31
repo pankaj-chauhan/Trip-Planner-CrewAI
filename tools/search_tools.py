a6002790450789a205eda79e26c3448170a1cefc
Pankaj Chauhan <pankajchauhan.687@gmail.com>
2024-08-31 13:29:14 +0530

    updated search.py

--- tools/search_tools.py
+++ tools/search_tools.py
@@ -9,4 +9,13 @@ class SearchTools():
 
     @tool("Search the internet")
     def search_internet(query):
+        """Useful to search the internet
+        about a a given topic and return relevant results"""
+        top_result_to_return = 4
+        url = "https://google.serper.dev/search"
+        payload = json.dumps({"q": query})
+        headers = {
+            'X-API-KEY': os.environ['SERPER_API_KEY'],
+            'content-type': 'application/json'
+        }
         
